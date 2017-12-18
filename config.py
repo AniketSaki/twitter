d68af27a0188ba77ce74292aa361b6b35041d771
AniketSaki <sakihenry@gmail.com>
2017-12-18 19:03:24 +0530

    Delete config.py
    encode values

--- config.py
+++
@@ -1,4 +0,0 @@
-consumerKey = 'zIAtajZejNYLURW48tEuXtO9p'
-consumerSecret = 'UP33mWTttsxlBn1GdOjfMu5LBgZ89AeTp8zSCMnqECRCsBseEi'
-accessToken = '433116932-U1T3OMkrnExXFnKULt8M8YFbAUCBf5LzuaUTRBY7'
-accessTokenSecret = 'yWquhABMKfImv2Cq5PCk8IA0i2s943bocKy4nSnJQ8HSG'
